9e4826126e6504691c9dce2f05be999d8c3679f5
unknown <SECK Balla@.(none)>
2012-02-15 22:53:06 +0100

    implmentation de quatre regles dans la classe Hand

--- src/main/java/poker/server/model/player/Hand.java
+++ src/main/java/poker/server/model/player/Hand.java
@@ -22,7 +22,30 @@ public class Hand {
 	private static final String NOT_FIVE_CARDS = "can't evaluate less than or more than five cards";
 	private static final int QUAD_SUIT_COUNT = 4;
 
+	
+	//from Cards
+	// the honors
+	public final static int KING = 13;
+	public final static int QUEEN = 12;
+	public final static int JACK = 11;
+	// the points
+	public final static int ACE = 1;
+	public final static int TWO = 2;
+	public final static int THREE = 3;
+	public final static int FOUR = 4;
+	public final static int FIVE = 5;
+	public final static int SIX = 6;
+	public final static int SEVEN = 7;
+	public final static int EIGHT = 8;
+	public final static int NINE = 9;
+	public final static int TEN = 10;
+	
+	
 	private List<Card> currentHand;
+
+	public List<Card> getCurrentHand() {
+		return currentHand;
+	}
 
 	protected Hand() {
 		currentHand = new ArrayList<Card>();
@@ -153,21 +176,113 @@ public class Hand {
 		}
 		return true;
 	}
-
+    //testé
 	public boolean isFullHouse() {
-		return true;
+		int card1 = currentHand.get(0).getValue();
+		int card2 = currentHand.get(1).getValue();
+		int card3 = currentHand.get(2).getValue();
+		int card4 = currentHand.get(3).getValue();
+		int card5 = currentHand.get(4).getValue();
+		
+		if((card1==card2)&&(card1==card3)&&(card1!=card4)&&(card4==card5))
+			return true;
+		if((card3==card2)&&(card4==card3)&&(card1!=card4)&&(card1==card5))
+			return true;
+		if((card3==card4)&&(card4==card5)&&(card1!=card4)&&(card1==card2))
+			return true;
+		if((card1==card3)&&(card3==card5)&&(card1!=card2)&&(card4==card2))
+			return true;
+		if((card1==card4)&&(card4==card5)&&(card1!=card3)&&(card3==card2))
+			return true;
+		if((card2==card4)&&(card4==card5)&&(card1!=card2)&&(card1==card3))
+			return true;
+		if((card1==card3)&&(card3==card4)&&(card1!=card2)&&(card5==card2))
+			return true;
+		if((card1==card2)&&(card1==card4)&&(card1!=card3)&&(card3==card5))
+			return true;
+		if((card1==card2)&&(card1==card5)&&(card1!=card4)&&(card4==card5))
+			return true;	
+		if((card2==card3)&&(card3==card5)&&(card1!=card2)&&(card1==card4))
+			return true;
+		
+		return false;
 	}
 
+	//testée
 	public boolean isFlush() {
-		return true;
+		if(!isStraightFlush()){
+			if(sameColor(currentHand))
+				return true;
+		}				
+		return false;
 	}
 
 	public boolean isStraight() {
-		return true;
+		
+		int nbColor = numberOfColor(currentHand);
+		//if the number of color >=2
+		if(nbColor>=2){
+			List<Card> l = currentHand;
+			List<Card> tempCards = sort(l);
+			int card1 = tempCards.get(0).getValue();
+			int card2 = tempCards.get(1).getValue();
+			int card3 = tempCards.get(2).getValue();
+			int card4 = tempCards.get(3).getValue();
+			int card5 = tempCards.get(4).getValue();
+			
+			if(card1==KING&&card2==QUEEN&&card3==JACK&&card4==TEN&&card5==NINE)
+				return true;
+			if(card1==QUEEN&&card2==JACK&&card3==TEN&&card4==NINE&&card5==EIGHT)
+				return true;
+			if(card1==JACK&&card2==TEN&&card3==NINE&&card4==EIGHT&&card5==SEVEN)
+				return true;
+			if(card1==TEN&&card2==NINE&&card3==EIGHT&&card4==SEVEN&&card5==SIX)
+				return true;
+			if(card1==NINE&&card2==EIGHT&&card3==SEVEN&&card4==SIX&&card5==FIVE)
+				return true;
+			if(card1==EIGHT&&card2==SEVEN&&card3==SIX&&card4==FIVE&&card5==FOUR)
+				return true;
+			if(card1==SEVEN&&card2==SIX&&card3==FIVE&&card4==FOUR&&card5==THREE)
+				return true;
+			if(card1==SIX&&card2==FIVE&&card3==FOUR&&card4==THREE&&card5==TWO)
+				return true;
+			if(card1==FIVE&&card2==FOUR&&card3==THREE&&card4==TWO&&card5==ACE)
+				return true;							
+		}
+		
+		return false;
 	}
-
+    //testée
 	public boolean isTrips() {
-		return true;
+		
+		int card1 = currentHand.get(0).getValue();
+		int card2 = currentHand.get(1).getValue();
+		int card3 = currentHand.get(2).getValue();
+		int card4 = currentHand.get(3).getValue();
+		int card5 = currentHand.get(4).getValue();
+		
+		if((card1==card2)&&(card1==card3)&&(card1!=card4)&&(card4!=card5))
+			return true;
+		if((card3==card2)&&(card4==card3)&&(card1!=card4)&&(card1!=card5))
+			return true;
+		if((card3==card4)&&(card4==card5)&&(card1!=card4)&&(card1!=card2))
+			return true;
+		if((card1==card3)&&(card3==card5)&&(card1!=card2)&&(card4!=card2))
+			return true;
+		if((card1==card4)&&(card4==card5)&&(card1!=card3)&&(card3!=card2))
+			return true;
+		if((card2==card4)&&(card4==card5)&&(card1!=card2)&&(card1!=card3))
+			return true;
+		if((card1==card3)&&(card3==card4)&&(card1!=card2)&&(card5!=card2))
+			return true;
+		if((card1==card2)&&(card1==card4)&&(card1!=card3)&&(card3!=card5))
+			return true;
+		if((card1==card2)&&(card1==card5)&&(card1!=card4)&&(card4!=card5))
+			return true;	
+		if((card2==card3)&&(card3==card5)&&(card1!=card2)&&(card1!=card4))
+			return true;
+		
+		return false;
 	}
 
 	public boolean isTwoPair() {
@@ -213,5 +328,60 @@ public class Hand {
 				++count;
 		}
 		return count;
+	}
+	
+	//count the number of colors
+	public int numberOfColor(List<Card> tempCards){
+		int heat=0;
+		int damond=0;
+		int club=0;
+		int spade=0;
+		for (Card card : tempCards) {
+			if(card.getSuit()=="Heart")
+				heat++;
+			if(card.getSuit()=="Diamond")
+				damond++;
+			if(card.getSuit()=="Club")
+				club++;
+			if(card.getSuit()=="Spade")
+				spade++;
+			
+		}
+		return heat+damond+club+spade;		
+		
+	}
+	
+	//test that cards are the same color
+	public boolean sameColor(List<Card> tempCards){
+		String color=tempCards.get(0).getSuit();
+		for (Card card : tempCards) {
+			if(card.getSuit()!=color)
+				return false;
+		}
+		return true;
+	}
+	
+	//return the number of occurrence of the current card
+	public int nbOccurence(List<Card>tempCards,Card currentCard){
+		int count=0;
+		for (Card card : tempCards) {
+			if(card.getValue()==currentCard.getValue())
+				count ++;
+		}
+		return count;
+	}
+	public List<Card> sort(List<Card> tempCards){
+		
+		for (int i = 0; i < tempCards.size(); i++) {
+			for (int j =1 ; j < tempCards.size(); j++) {
+				if(tempCards.get(i).getValue()<tempCards.get(j).getValue()){
+					Card temp = tempCards.get(i);
+					tempCards.set(i, tempCards.get(j));
+					tempCards.set(j, temp);
+			    }
+		    }
+		}
+		
+		return tempCards;
 	}
 }
