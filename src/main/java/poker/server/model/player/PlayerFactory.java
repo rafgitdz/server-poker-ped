51accc2e3c8daab5ec5ec6ef7932bcdc7e2eaef1
unknown <Lucas@.(none)>
2012-02-16 16:41:05 +0100

    ajout player service

---
+++ src/main/java/poker/server/model/player/PlayerFactory.java
@@ -0,0 +1,21 @@
+package poker.server.model.player;
+
+import javax.ejb.EJB;
+
+public class PlayerFactory implements PlayerFactoryLocal {
+	  
+	@EJB
+    private RepositoryPlayer repositoryPlayer;
+
+	@Override
+	public Player authentificate(String name, String pwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Player createUser(String name, String pwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/main/java/poker/server/service/PlayerServiceRemote.java
@@ -0,0 +1,10 @@
+package poker.server.service;
+
+import poker.server.model.player.Player;
+
+public interface PlayerServiceRemote {
+	
+	public Player authentificate(String name, String pwd);
+	
+	public Player createUser(String name, String pwd);
+}
